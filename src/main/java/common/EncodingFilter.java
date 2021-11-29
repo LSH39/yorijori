c7d8f3c5ff171abda82a680af8a4b6a620ccc613
LEE SOHYEON <blanche309@naver.com>
2021-11-29 17:22:06 +0900

    - filter 추가

---
+++ src/main/java/common/EncodingFilter.java
@@ -0,0 +1,47 @@
+package common;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/EncodingFilter")
+public class EncodingFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
