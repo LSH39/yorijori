9256fb4dd55e9423aacde55146c2b47fe899abed
hyowon <kkhw9750@naver.com>
2021-11-30 13:31:00 +0900

    효원패키지(우승작,팔로우)

---
+++ src/main/java/kr/or/mypage/service/MypageService.java
@@ -0,0 +1,5 @@
+package kr.or.mypage.service;
+
+public class MypageService {
+
+}
---
+++ src/main/java/kr/or/mypage/vo/Follow.java
@@ -0,0 +1,11 @@
+package kr.or.mypage.vo;
+
+import lombok.Data;
+@Data
+public class Follow {
+
+	private int followingNo;
+	private int memberNo;
+	private int followNo;
+
+}
