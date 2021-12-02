6b583e7a50405e63a34357c667aa2a4ddb12b33a
seungmin <forestwow@protonmail.ch>
2021-12-02 18:29:02 +0900

    프로필 / 쿠킹클래스 페이지 작성

--- src/main/java/kr/or/cookingCls/controller/CookingClsController.java
+++ src/main/java/kr/or/cookingCls/controller/CookingClsController.java
@@ -1,5 +1,38 @@
 package kr.or.cookingCls.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.or.cookingCls.model.service.CookingClsService;
+
+@Controller
 public class CookingClsController {
 
+	@Autowired
+	private CookingClsService service;
+	
+	//쿠킹클래스 리스트 페이지
+	@RequestMapping(value="/cookingClsList.do")
+	public String CookingClsList() {
+		return "cookingcls/cookingClsList";
+	}
+	
+	//쿠킹클래스 작성
+	@RequestMapping(value="/cookingClsWriteFrm.do")
+	public String CookingClsListWriteFrm() {
+		return "cookingcls/cookingClsWriteFrm";
+	}
+	
+	//쿠킹클래스 조회
+	@RequestMapping(value="/cookingClsView.do")
+	public String CookingClsListView() {
+		return "cookingcls/cookingClsView";
+	}
+	
+	//쿠킹클래스 수정
+	@RequestMapping(value="/cookingClsUpdateFrm.do")
+	public String CookingClsListUpdateFrm() {
+		return "cookingcls/cookingClsUpdateFrm";
+	}
 }
