c940b1f1a9712c8a9de580abc7cca5099b0764d2
kimminji <alclssys340@gmail.com>
2021-12-02 18:23:06 +0900

    레시피 폼 작성 및 FileVo클래스 생성

--- src/main/java/kr/or/recipe/model/dao/RecipeDao.java
+++ src/main/java/kr/or/recipe/model/dao/RecipeDao.java
@@ -7,7 +7,9 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import kr.or.recipe.model.vo.Material;
 import kr.or.recipe.model.vo.RecipeBoard;
+import kr.or.recipe.model.vo.RecipeContent;
 
 
 @Repository
@@ -19,4 +21,23 @@ public class RecipeDao {
 		List<RecipeBoard>list = sqlSession.selectList("recipe.selectRecipeList"); 
 		return (ArrayList<RecipeBoard>)list;
 	}
+
+	public int insertRecipeBoard(RecipeBoard rb) {
+		return sqlSession.insert("recipe.insertRecipeBoard", rb);
+		 
+	}
+
+	public int selectRecipeNo(RecipeBoard rb) {
+		int result = sqlSession.selectOne("recipe.selectRecipeNo");
+		return result;
+	}
+	public int insertMaterial(Material m) {
+		return sqlSession.insert("recipe.insertMaterial",m);
+	}
+
+	public int insertContent(RecipeContent rc) {
+		return sqlSession.insert("recipe.insertContent", rc);
+	}
+
+
 }
