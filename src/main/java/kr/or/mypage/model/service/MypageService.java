c7a2730959ebd630b01a76bd7419dc9b4edf6d4a
hyowon <kkhw9750@naver.com>
2021-12-03 17:56:49 +0900

    마이페이지 조회내역들 (후기,클래스,레시피)

--- src/main/java/kr/or/mypage/model/service/MypageService.java
+++ src/main/java/kr/or/mypage/model/service/MypageService.java
@@ -1,5 +1,7 @@
 package kr.or.mypage.model.service;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import kr.or.coupon.model.vo.MyCoupon;
 import kr.or.member.model.vo.Member;
 import kr.or.mypage.model.dao.MypageDao;
 import kr.or.recipe.model.vo.RecipeBoard;
+import kr.or.review.model.vo.MyClassReview;
 import kr.or.review.model.vo.Review;
 @Service
 public class MypageService {
@@ -29,30 +32,54 @@ public class MypageService {
 		return result;
 	}
 
-
-	public Review classReview(String memberId) {
-		Review r = dao.classReview(memberId);
-		return r;
-	}
-
+/*
 	public MyCoupon mycoupon(int memberNo) {
 		MyCoupon mc =dao.myCoupon(memberNo);
 		return mc;
 	}
-
+	*/
+/*
 	public RecipeBoard myRecipe(int recipeWriter) {
 		RecipeBoard re= dao.myRecipe(recipeWriter);
 		return re;
 	}
-
+*/
 	public Member sellerPage(String memberId) {
 		Member m =dao.sellerPage(memberId);
 		return m;
 	}
-
+/*
 	public MyCookingRsrv myclass(int memberNo) {
 		MyCookingRsrv mcr =dao.myClass(memberNo);
 		return mcr;
 	}
+*/
+	/*
+	public MyClassReview myclassReview(int memberNo) {
+		MyClassReview mcrv =dao.myClassReview(memberNo);
+		return null;
+	}
+*/
+	public ArrayList<RecipeBoard> myRecipe(int recipeWriter) {
+		ArrayList<RecipeBoard> list = dao.myRecipe(recipeWriter);
+		return list;
+	}
+
+	public ArrayList<MyCoupon> mycoupon(int memberNo) {
+		ArrayList<MyCoupon> list = dao.myCoupon(memberNo);
+		return list;
+	}
+
+	public ArrayList<MyCookingRsrv> myclass(int memberNo) {
+		ArrayList<MyCookingRsrv> list =dao.myClass(memberNo);
+		return list;
+	}
+
+	public ArrayList<MyClassReview> myclassReview(int memberNo) {
+		ArrayList<MyClassReview> list =dao.myClassReview(memberNo);
+		return list;
+	}
+
+	
 
 }
