e7c39ec23e3e290897c6cd005931ae7484c8c55e
seungmin <forestwow@protonmail.ch>
2021-12-06 19:10:58 +0900

    요리클래스 삭제, 리뷰 작성(AJAX), 리뷰 삭제

--- src/main/java/kr/or/review/model/dao/ReviewDao.java
+++ src/main/java/kr/or/review/model/dao/ReviewDao.java
@@ -13,12 +13,24 @@ import kr.or.review.model.vo.Review;
 public class ReviewDao {
 
 	@Autowired
-	private SqlSessionTemplate session;
+	private SqlSessionTemplate sqlSession;
 
+	//클래스에 대한 리뷰 전체 조회
 	public ArrayList<Review> selectAllReview(int classNo) {
 		// TODO Auto-generated method stub
-		List<Review> list = session.selectList("review.selectAllReview", classNo);
+		List<Review> list = sqlSession.selectList("review.selectAllReview", classNo);
 		return (ArrayList<Review>)list;
+	}
+
+	//리뷰 작성
+	public int insertReview(Review review) {
+		// TODO Auto-generated method stub
+		return sqlSession.insert("review.insertReview", review);
+	}
+
+	public int deleteReview(int reviewNo) {
+		// TODO Auto-generated method stub
+		return sqlSession.delete("review.deleteReview", reviewNo);
 	}
 	
 	
