86b828b6cdd0d895aa1189475ad0a63bc6fcfdad
LEE SOHYEON <blanche309@naver.com>
2021-12-06 11:29:41 +0900

    - 로그인 임시 / 로그아웃

--- src/main/java/kr/or/member/controller/MemberController.java
+++ src/main/java/kr/or/member/controller/MemberController.java
@@ -1,14 +1,40 @@
 package kr.or.member.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.member.model.service.MemberService;
+import kr.or.member.model.vo.Member;
 
 @Controller
 public class MemberController {
 	@Autowired
 	private MemberService service;
 	
+	@RequestMapping(value="/loginFrm.do")
+	public String loginFrm() {
+		return "member/loginFrm";
+	}
+	@RequestMapping(value="/login.do")
+	public String login(Member member, HttpSession session, Model model) {
+		Member m = service.loginMember(member);
+		if(m != null) {
+			session.setAttribute("m", m);
+			return "redirect:/";
+		}else {
+			model.addAttribute("msg", "아이디 또는 비밀번호를 확인해주세요.");
+			model.addAttribute("loc","/loginFrm.do");
+		}
+		return "common/msg";
+	}
 	
+	@RequestMapping(value="/logout.do")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}
 }
