32a445c62c1a6c614ce058a031966140be3ed156
LEE SOHYEON <blanche309@naver.com>
2021-12-10 19:10:47 +0900

    - 1210 cart img etc..

--- src/main/java/kr/or/cart/model/vo/Cart.java
+++ src/main/java/kr/or/cart/model/vo/Cart.java
@@ -1,12 +1,32 @@
 package kr.or.cart.model.vo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
 	private int cartNo;
 	private int memberNo;
 	private int productNo;
 	private int productAmount;
 	private int productPrice;
+	// 추가
+	private String sellerNickname;
+	private String milkitName;
+	private int milkitPrice;
+	private String productFilepath;
+	
+	public Cart(int cartNo, int memberNo, int productNo, int productAmount, int productPrice) {
+		super();
+		this.cartNo = cartNo;
+		this.memberNo = memberNo;
+		this.productNo = productNo;
+		this.productAmount = productAmount;
+		this.productPrice = productPrice;
+	}
+	
+	
 }
