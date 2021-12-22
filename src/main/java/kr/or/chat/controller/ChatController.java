ce064f6df7e5ea08be56dbd4ce3e70543f89f49a
LEE SOHYEON <blanche309@naver.com>
2021-12-22 18:55:01 +0900

    - chat 수정

--- src/main/java/kr/or/chat/controller/ChatController.java
+++ src/main/java/kr/or/chat/controller/ChatController.java
@@ -18,53 +18,6 @@ import kr.or.member.model.vo.Member;
 public class ChatController {
 	@Autowired
 	private ChatService service;
-	
-//	// 조회
-//	@ResponseBody
-//	@RequestMapping(value="/chatUserFrm.do")
-//	public ArrayList<Chat> chatUserFrm(HttpSession session) {
-//		if(session.getAttribute("m")!=null) {
-//			Member sessionMember = (Member)session.getAttribute("m");
-//			int sessionMemberNo = sessionMember.getMemberNo();
-//			ArrayList<Chat> chatUserList = service.chatUserList(sessionMemberNo);
-//			return chatUserList;
-//		}
-//		else {
-//			return null;
-//		}
-//	}
-//	
-//	@ResponseBody
-//	@RequestMapping(value="/chatAdminHomeFrm.do")
-//	public ArrayList<Chat> chatAdminHomeFrm(HttpSession session) {
-//		if(session.getAttribute("m")!=null) {
-//			Member sessionMember = (Member)session.getAttribute("m");
-//			int sessionMemberNo = sessionMember.getMemberNo();
-//			ArrayList<Chat> chatAdminHomeList = service.chatAdminHomeList(sessionMemberNo);
-//			return chatAdminHomeList;
-//		}else {
-//			return null;
-//		}
-//	}
-//	
-//	@ResponseBody
-//	@RequestMapping(value="/chatAdminFrm.do")
-//	public ArrayList<Chat> chatAdminFrm(String selectUser, String sessionMemberNo) {
-//		Chat chat = new Chat();
-//		chat.setMemberNickname(selectUser);
-//		chat.setChatReceive(Integer.parseInt(sessionMemberNo));
-//		ArrayList<Chat> chatAdminList = service.chatAdminList(chat);
-//		return chatAdminList;
-//	}
-//	
-//	// insert
-//	@ResponseBody
-//	@RequestMapping(value="/chatSend.do")
-//	public int chatSend(Chat chat) {
-//		int result = service.chatSend(chat);
-//		return result;
-//	}
-	
 
 	
 }
