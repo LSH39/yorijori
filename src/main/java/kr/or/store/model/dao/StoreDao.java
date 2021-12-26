105e566e0b33ad283be34cc0d739632072be9b36
seungmin <forestwow@protonmail.ch>
2021-12-27 03:14:28 +0900

    오프라인 매장 페이지 추가

--- src/main/java/kr/or/store/model/dao/StoreDao.java
+++ src/main/java/kr/or/store/model/dao/StoreDao.java
@@ -1,5 +1,12 @@
 package kr.or.store.model.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class StoreDao {
 
+	@Autowired
+	private SqlSessionTemplate session;
 }
--- src/main/java/kr/or/store/model/service/StoreService.java
+++ src/main/java/kr/or/store/model/service/StoreService.java
@@ -1,5 +1,13 @@
 package kr.or.store.model.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.or.store.model.dao.StoreDao;
+
+@Service
 public class StoreService {
 
+	@Autowired
+	private StoreDao dao;
 }
